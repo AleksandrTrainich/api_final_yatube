87eafd370e4b399ead4949844bd3074e330416fb
Александр Трайнич <a.trainich@gmail.com>
2021-04-18 14:21:59 +0300

    2 commit

--- api/serializers.py
+++ api/serializers.py
@@ -1,6 +1,9 @@
 from rest_framework import serializers
+from rest_framework.validators import UniqueTogetherValidator
 
-from .models import Comment, Post
+from .models import Comment, Post, Follow, Group
+from django.contrib.auth import get_user_model
+User = get_user_model()
 
 
 class PostSerializer(serializers.ModelSerializer):
@@ -17,3 +20,29 @@ class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         fields = ('id', 'author', 'post', 'text', 'created')
         model = Comment
+
+
+class FollowSerializer(serializers.ModelSerializer):
+    user = serializers.SlugRelatedField(
+        slug_field='username',
+        default=serializers.CurrentUserDefault(),
+        queryset=User.objects.all())
+    following = serializers.SlugRelatedField(slug_field='username',
+                                             queryset=User.objects.all())
+
+    class Meta:
+        fields = ('user', 'following')
+        model = Follow
+        validators = [UniqueTogetherValidator(
+            queryset=Follow.objects.all(), fields=['user', 'following'])]
+
+    def validate(self, data):
+        if data['user'] == data['following']:
+            raise serializers.ValidationError("Нельзя подписаться на себя")
+        return data
+
+
+class GroupSerializer(serializers.ModelSerializer):
+    class Meta:
+        fields = ('id', 'title')
+        model = Group
--- api/urls.py
+++ api/urls.py
@@ -1,4 +1,18 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,)
+from .views import PostViewSet, CommentViewSet, FollowViewSet, GroupViewSet
 
+router = DefaultRouter()
+router.register('posts', PostViewSet, basename='Posts')
+router.register('group', GroupViewSet, basename='Groups')
+router.register('follow', FollowViewSet, basename='Follows')
+router.register(r'posts/(?P<post_id>\d+)/comments', CommentViewSet,
+                basename='Comments')
 urlpatterns = [
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('', include(router.urls)),
 ]
